c1751040aae937e6acfece58535e06af5e5115e1
hoangnhat42 <hoangnhat.contact@gmail.com>
2022-07-23 09:09:19 +0700

    route and page  again

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,13 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
-import "antd/dist/antd.css";
 
 ReactDOM.render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
