97e235dd79d5c93a6f636c9c19aca8c91e8e30bc
hoangnhat42 <hoangnhat.contact@gmail.com>
2022-07-23 09:11:39 +0700

    Ant design Navigation

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,14 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
+import "antd/dist/antd.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  // </React.StrictMode>,
   document.getElementById("root")
 );
 
